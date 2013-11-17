286ae9ef836e64d2ba5befadbeae6359ea355600
Alastair M. Robinson <robinsonb5@talk21.com>
2013-11-17 18:58:39 +0000

    Started adding Minimig support.

--- ModPlayer/Software/fat.c
+++ ModPlayer/Software/fat.c
@@ -112,13 +112,15 @@ void SwapPartitionBytes(int i)
 }
 
 #define BootPrint(x) puts(x);
-
+#define bprintfl small_printf
+#if 0
 void bprintfl(const char *fmt,unsigned long l)
 {
 	char s[64];
 	sprintf(s,fmt,l);
 	BootPrint(s);
 }
+#endif
 
 // FindDrive() checks if a card is present and contains FAT formatted primary partition
 unsigned int FindDrive(void)
@@ -154,20 +156,21 @@ unsigned int FindDrive(void)
 				SwapPartitionBytes(1);
 				SwapPartitionBytes(2);
 				SwapPartitionBytes(3);
+				BootPrint("Done\n");
 				// fall through...
 			case 0xaa55:
 				// get start of first partition
 				boot_sector = partitions[0].startlba;
-				bprintfl("Start: %ld\n",partitions[0].startlba);
+				bprintfl("Start: %d\n",partitions[0].startlba);
 				for(partitioncount=4;(partitions[partitioncount-1].sectors==0) && (partitioncount>1); --partitioncount)
 					;
-				bprintfl("PartitionCount: %ld\n",partitioncount);
+				bprintfl("PartitionCount: %d\n",partitioncount);
 				int i;
 				for(i=0;i<partitioncount;++i)
 				{
-					bprintfl("Partition: %ld",i);
-					bprintfl("  Start: %ld",partitions[i].startlba);
-					bprintfl("  Size: %ld\n",partitions[i].sectors);
+					bprintfl("Partition: %d",i);
+					bprintfl("  Start: %d",partitions[i].startlba);
+					bprintfl("  Size: %d\n",partitions[i].sectors);
 				}
 				if (!sd_read_sector(boot_sector, sector_buffer)) // read discriptor
 				    return(0);
@@ -274,14 +277,16 @@ unsigned int FindDrive(void)
 
 
     // some debug output
-    printf("fat_size: %lu\r", fat_size);
-    printf("fat_number: %u\r", fat_number);
-    printf("fat_start: %lu\r", fat_start);
-    printf("root_directory_start: %lu\r", root_directory_start);
-    printf("dir_entries: %u\r", dir_entries);
-    printf("data_start: %lu\r", data_start);
-    printf("cluster_size: %u\r", cluster_size);
-    printf("cluster_mask: %08lX\r", cluster_mask);
+#ifdef DEBUG
+    printf("fat_size: %d\r", fat_size);
+    printf("fat_number: %d\r", fat_number);
+    printf("fat_start: %d\r", fat_start);
+    printf("root_directory_start: %d\r", root_directory_start);
+    printf("dir_entries: %d\r", dir_entries);
+    printf("data_start: %d\r", data_start);
+    printf("cluster_size: %d\r", cluster_size);
+    printf("cluster_mask: %d\r", cluster_mask);
+#endif
 
     return(1);
 }
@@ -988,7 +993,7 @@ int ScanDirectory(unsigned long mode, char *extension, unsigned char options)
     }
     /*
     time = GetTimer(0) - time;
-    printf("ScanDirectory(): %lu ms\r", time >> 20);
+    printf("ScanDirectory(): %d ms\r", time >> 20);
     */
     return rc;
 }
@@ -1241,7 +1246,7 @@ unsigned int FileCreate(unsigned long iDirectory, fileTYPE *file)
 
             if (pEntry->Name[0] == SLOT_EMPTY)
             {
-                printf("Empty entry found in sector %lu at index %lu\r", iDirectorySector-1, iEntry&0x0F);
+                printf("Empty entry found in sector %d at index %d\r", iDirectorySector-1, iEntry&0x0F);
 
                 // free cluster is marked as 0x0000
                 // last cluster in chain is 0xFFFF
@@ -1269,7 +1274,7 @@ unsigned int FileCreate(unsigned long iDirectory, fileTYPE *file)
                         {   // empty cluster found
                             unsigned long cluster = (fat_index << (fat32 ? 7 : 8)) + buffer_index;  // calculate cluster number
 
-                            printf("Empty cluster: %lu\r", cluster);
+                            printf("Empty cluster: %d\r", cluster);
 
                             // mark cluster as used
                             if (fat32)
@@ -1291,7 +1296,7 @@ unsigned int FileCreate(unsigned long iDirectory, fileTYPE *file)
                             {
                                 if (!sd_write_sector(fat_start + (i * fat_size) + fat_index, (unsigned char*)&fat_buffer))
                                 {
-                                    printf("FileCreate(): FAT copy #%lu write failed!\r", i);
+                                    printf("FileCreate(): FAT copy #%d write failed!\r", i);
                                     return(0);
                                 }
                             }
@@ -1376,9 +1381,9 @@ unsigned int UpdateEntry(fileTYPE *file)
     if ((SwapBBBB(pEntry->FileSize) + cluster_size - 1) / (cluster_size << 9) != (file->size + cluster_size - 1) / (cluster_size << 9))
     {
         printf("UpdateEntry(): different number of clusters!\r");
-        printf("pEntry->FileSize = %lu\r", SwapBBBB(pEntry->FileSize));
-        printf("file->size = %lu\r", file->size);
-        printf("cluster_size = %u\r", cluster_size);
+        printf("pEntry->FileSize = %d\r", SwapBBBB(pEntry->FileSize));
+        printf("file->size = %d\r", file->size);
+        printf("cluster_size = %d\r", cluster_size);
         return(0);
     }
 
---
+++ RTL/MinimigToplevel.vhd
@@ -0,0 +1,127 @@
+-- Minimig toplevel file
+-- 
+-- This file is part of Minimig
+-- 
+-- Minimig is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+-- 
+-- Minimig is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <http:-- www.gnu.org/licenses/>.
+--
+-- Toplevel converted to VHDL by Alastair M. Robinson
+
+library ieee;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+entity MinimigToplevel is
+port (
+	-- m68k pins
+	cpu_data : inout std_logic_vector(15 downto 0);	-- m68k data bus
+	cpu_address : in std_logic_vector(23 downto 1);	-- m68k address bus
+	n_cpu_ipl  : out std_logic_vector(2 downto 0);		-- m68k interrupt request
+	n_cpu_as : in std_logic;			-- m68k address strobe
+	n_cpu_uds : in std_logic;			-- m68k upper data strobe
+	n_cpu_lds : in std_logic;			-- m68k lower data strobe
+	cpu_r_w : in std_logic;			-- m68k read / write
+	n_cpu_dtack : out std_logic;			-- m68k data acknowledge
+	n_cpu_reset : inout std_logic;			-- m68k reset
+	cpu_clk : out std_logic;			-- m68k clock
+	-- sram pins
+	ram_data : inout std_logic_vector(15 downto 0);	-- sram data bus
+	ram_address : out std_logic_vector(19 downto 1);	-- sram address bus
+	n_ram_ce : out std_logic_vector(3 downto 0);		-- sram chip enable
+	n_ram_bhe : out std_logic;			-- sram upper byte select
+	n_ram_ble : out std_logic;			-- sram lower byte select
+	n_ram_we : out std_logic;			-- sram write enable
+	n_ram_oe : out std_logic;			-- sram  enable
+	-- system	pins
+	mclk  : in std_logic;				-- master system clock (4.433619MHz)
+	-- rs232 pins
+	rxd : in std_logic;				-- rs232 receive
+	txd : out std_logic;			-- rs232 send
+	cts : in std_logic;				-- rs232 clear to send
+	rts : out std_logic;				-- rs232 request to send
+	-- I/O
+	n_joy1 : in std_logic_vector(5 downto 0);			-- joystick 1 [fire1,fire,up,down,left,right] (default mouse port)
+	n_joy2 : in std_logic_vector(5 downto 0);			-- joystick 2 [fire1,fire,up,down,left,right] (default joystick port)
+	n_15khz : in std_logic;				-- scandoubler disable
+	pwrled : out std_logic;				-- power led
+	msdat : inout std_logic;				-- PS2 mouse data
+	msclk : inout std_logic;				-- PS2 mouse clk
+	kbddat : inout std_logic;				-- PS2 keyboard data
+	kbdclk : inout std_logic;				-- PS2 keyboard clk
+	-- host controller interface (SPI)
+	n_scs : in std_logic_vector(2 downto 0);			-- SPI chip select
+	sdi : in std_logic;				-- SPI data input
+	sdo : inout std_logic;				-- SPI data 
+	sck : in std_logic;				-- SPI clock
+	-- video
+	n_hsync : out std_logic;				-- horizontal sync
+	n_vsync : out std_logic;				-- vertical sync
+	red : out std_logic_vector(3 downto 0);			-- red
+	green : out std_logic_vector(3 downto 0);		-- green
+	blue : out std_logic_vector(3 downto 0);			-- blue
+	-- audio
+	left : out std_logic;				-- audio bitstream left
+	right : out std_logic;				-- audio bitstream right
+	-- user i/o
+	drv_snd : out std_logic;
+	-- unused pins
+	init_b : out std_logic				-- vertical sync for MCU (sync OSD update)
+);
+end entity;
+
+architecture RTL of MinimigToplevel is
+
+signal sysclk : std_logic;
+signal clklocked : std_logic;
+
+begin
+
+n_ram_oe<='1';
+cpu_data<=(others=>'Z');
+n_cpu_ipl<=(others=>'1');
+n_cpu_dtack<='1';
+cpu_clk<='0';
+ram_address<=(others => '0');
+n_ram_ce<=(others => '1');
+n_ram_bhe<='1';
+n_ram_ble<='1';
+n_ram_we<='1';
+n_ram_oe<='1';
+txd<='1';
+rts<='1';
+pwrled<='1';
+msdat<='Z';
+msclk<='Z';
+kbddat<='Z';
+kbdclk<='Z';
+sdo<='Z';
+n_hsync<='1';
+n_vsync<='1';
+red<=(others => '0');
+green<=(others => '0');
+blue<=(others => '0');
+left<='0';
+right<='0';
+drv_snd<='0';
+init_b<='0';
+
+
+myclock : entity work.sysclock
+port map(
+	CLKIN_IN => mclk,
+	RST_IN => '0',
+	CLKFX_OUT => sysclk,
+	LOCKED_OUT => clklocked
+);
+
+end architecture;
